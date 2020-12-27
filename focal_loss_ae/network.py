00de218dd10ca11546156c3826fd1cd935fcc083
Lisheng <listenonepiece@gmail.com>
2020-12-27 11:22:50 +0800

    network forward finished

--- focal_loss_ae/network.py
+++ focal_loss_ae/network.py
@@ -3,11 +3,13 @@ from torch import nn
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, z_size):
+        self.encoder = None
+        self.decoder = None
+        self.z_size = z_size
+        self._build_networks()
 
     def _build_networks(self):
-              h = tf.layers.conv2d(self.x, 32, 4, strides=2,
-                                   activation=tf.nn.relu, name="enc_conv1")
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, dilation=2, stride=2, padding=2),
             nn.relu(),
@@ -18,21 +20,34 @@ class AutoEncoder(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3),
             nn.relu(),
             nn.Conv2d(64, 64, kernel_size=3)
-        ) # 64 - 32 - 16 - 8 - 6 - 4
+        )  # 64 - 32 - 16 - 8 - 6 - 4
+
+        self.mu = nn.Linear(1024, self.z_size)
+        self.logvar = nn.Linear(1024, self.z_size)
+
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 64, kernel_size=3), # 6
+            nn.ConvTranspose2d(64, 64, kernel_size=3),  # 6
             nn.relu(),
-            nn.ConvTranspose2d(64, 64, kernel_size=3), # 8
+            nn.ConvTranspose2d(64, 64, kernel_size=3),  # 8
             nn.relu(),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, dilation=2, stride=2, output_padding=1), # 16
+            nn.ConvTranspose2d(64, 32, kernel_size=3, dilation=2,
+                               stride=2, output_padding=1),  # 16
             nn.relu(),
-            nn.ConvTranspose2d(32, 32, kernel_size=3, dilation=2, stride=2, output_padding=1), # 32
+            nn.ConvTranspose2d(32, 32, kernel_size=3, dilation=2,
+                               stride=2, output_padding=1),  # 32
             nn.relu(),
-            nn.ConvTranspose2d(32, 3, kernel_size=3, dilation=2, stride=2, output_padding=1),
+            nn.ConvTranspose2d(32, 3, kernel_size=3,
+                               dilation=2, stride=2, output_padding=1),
             nn.sigmoid()
         )
-    
-
-
 
     def forward(self, x):
+        e = self.encoder(x)
+        print("the input after the encoder network", e.shape)
+        e = torch.reshape(e, (-1, 1024))
+        mu = self.mu(e)
+        logvar = self.logvar(e)
+        sigma = torch.exp(logvar/2.)
+        z = mu + torch.randn_like(sigma) * sigma
+        output = self.decoder(z)
+        return output
